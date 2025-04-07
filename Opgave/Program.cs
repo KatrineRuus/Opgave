5dc4bfeea94bb3939ce8119d79dc1f050939029f
Bruger <Bruger@LAPTOP-H0NNLFDR>
2025-04-07 23:29:23 +0200

    Add project files.

---
+++ Opgave/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Tomater = 3, Æbler = 5, Bananer = 2;
+            string Brød = "Brød";
+            //Allan medbringer brød.Per medbringer 3 tomater, Lise medbringer 5 æbler og Kim medbringer 2 bananer.
+            Console.WriteLine(" Allan med bringer {0}, Per medbringer {1} Tomater, Lise medbringer {2} æbler, Kim medbringer {3} bananer.",Brød,Tomater,Æbler,Bananer);
+            Console.ReadKey();
+        }
+    }
+}
